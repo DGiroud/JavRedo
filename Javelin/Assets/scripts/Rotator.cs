457e686f2c7280a20c992dbc7f2f584f5afb2b0a
DGiroud <39175181+DGiroud@users.noreply.github.com>
2019-06-21 11:20:06 +1000

    //

---
+++ Javelin/Assets/scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float multiplyer;
+
+
+
+    private void Update()
+    {
+        transform.Rotate(0,Time.deltaTime/ multiplyer, 0);
+    }
+}
---
+++ Javelin/Assets/scripts/Sound_Manager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Manager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct SoundEffect
+    {
+        public AudioClip sound;
+
+        [Range(0,1)]
+        public float volume;
+
+        [Range(0, 1)]
+        public float pitch;
+    }
+
+    public AudioSource BGMSource;
+    public AudioSource SFXSource;
+
+    [Space]
+    public SoundEffect[] soundEffects;
+
+    public SoundEffect[] backgroundEffects;
+
+    private bool isStarted = false;
+    private int lastBGM;
+
+    private void Awake()
+    {
+        isStarted = true;
+        PlayBGM(0);
+    }
+
+
+    private void Update()
+    {
+        if (!isStarted && !BGMSource.isPlaying)
+        {
+            isStarted = true;
+
+            int rand = Random.Range(0, backgroundEffects.Length);
+            while (rand == lastBGM)            
+                rand = Random.Range(0, backgroundEffects.Length);            
+
+            lastBGM = rand;
+            PlayBGM(rand);
+        }
+    }
+
+    public void PlaySound(int effect)
+    {
+        SFXSource.clip = soundEffects[effect].sound;
+        SFXSource.volume = soundEffects[effect].volume;
+        SFXSource.pitch = soundEffects[effect].pitch;
+
+        SFXSource.Play();
+    }
+
+
+    void PlayBGM(int effect)
+    {
+        BGMSource.clip = backgroundEffects[effect].sound;
+        BGMSource.volume = backgroundEffects[effect].volume;
+        BGMSource.pitch = backgroundEffects[effect].pitch;
+
+        BGMSource.Play();
+        isStarted = false;
+    }
+
+}
